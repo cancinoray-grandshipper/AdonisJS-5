d25f34ed8ebfbbba4084d40017fb78607dd53cc3
cancinoray <cancinoray@gmail.com>
2022-07-11 09:19:38 +0800

    First Commit

---
+++ demo-adonis/app/Controllers/Http/PetsController.ts
@@ -0,0 +1,11 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PetsController {
+    public async index() {
+        return 'GET pets';
+    }
+
+    public async store() {
+        return 'POST pet'
+    }
+}
